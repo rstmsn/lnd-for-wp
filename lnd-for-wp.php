cc2fa89a59b1e49c01acb1c26318dc1ef8966182
rstmsn <rosstitute@gmail.com>
2019-03-11 00:39:33 +0000

    Update lnd-for-wp.php

--- lnd-for-wp.php
+++ lnd-for-wp.php
@@ -17,7 +17,7 @@
  * Plugin URI:        http://github.com/rstmsn/lnd-for-wp/
  * Description:       Manage and use your LND node.
  * Version:           0.1.0
- * Author:            Ross Thompson
+ * Author:            RSTMSN
  * Author URI:        http://github.com/rstmsn/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
